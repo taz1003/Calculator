5b37c41d5b0d3656d9e44de13de310b79c79c0e1
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2025-01-11 19:32:32 +0000

    Update mainCalculate function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,34 @@
 const display = document.getElementById("calc-display"); // Select display
 const buttons = document.querySelectorAll("buttons-id buttons"); // selects all buttons
 
+let error = false
+
 for (let button of buttons) {
     button.addEventListener("click", () => {
         let value = button.textContent;
     })
 }
+
+function mainCalculate() {
+
+    let result
+    if (operator === "+") {
+        result = num1 + num2;
+    } else if (operator === "-") {
+        result = num1 - num2;
+    } else if (operator === "x") {
+        result = num1 * num2;
+    } else if (operator === "÷") {
+        result = num2 !== 0 ? num1/num2 : "Error" // Shows error if dividing by zero
+    } else {
+        result = "Invalid Operator" // Handle invalid operators
+    }
+}
+
+
+function updateDisplay()
+
+
+function clearCalculator()
+
+
