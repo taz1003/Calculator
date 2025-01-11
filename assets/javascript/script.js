9e397179b819c95041f86fa3b95fbacffb7af0ee
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2025-01-11 18:19:29 +0000

    Add basic DOM elements

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
+const display = document.getElementById("calc-display"); // Select display
+const buttons = document.querySelectorAll("buttons-id buttons"); // selects all buttons
 
-    
-})
+for (let button of buttons) {
+    button.addEventListener("click", () => {
+        let value = button.textContent;
+    })
+}
