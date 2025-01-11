cbb08b50ff5e92a93b8b67a78ddc0b3177edcbe2
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2025-01-11 20:04:34 +0000

    Create iterations for number and operator buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,18 +6,32 @@ const operatorButtons = document.querySelectorAll(".button-operator");
 const decimalButton = document.getElementById("decimal");
 const clearButton = document.getElementById("button-clear");
 
-let error = false
+// Initialize variables for the calculator state
+let error = false; // error state
+let currentInput = ""; // stores the latest input
+let num1 = null; // first number
+let operator = null; // operators
 
-
-
+// Iterating through all the number buttons
 for (let button of numberButtons) {
     button.addEventListener("click", () => {
         let value = button.textContent;
+        appendNumber()
     });
 };
 
-function mainCalculate() {
+// Iterating through all the operator buttons
+for (let button of operatorButtons) {
+    button.addEventListener("click" => (
+        let value = button.textContent;
+        setOperator()
+    ));
+};
 
+/**
+ * Calculates 
+ */
+function mainCalculate() {
     let result
     if (operator === "+") {
         result = num1 + num2;
@@ -29,15 +43,27 @@ function mainCalculate() {
         result = num2 !== 0 ? num1/num2 : "Error" // Shows error if dividing by zero
     } else {
         result = "Invalid Operator" // Handle invalid operators
-    }
-}
+    };
+};
 
 
-function updateDisplay() {
-    
-}
+/**
+ * 
+ */
+function appendNumber()
+
+/**
+ * 
+ */
+function setOperator()
 
 
+/**
+ * 
+ */
+function updateDisplay()
+
+/**
+ * 
+ */
 function clearCalculator()
-
-
